d3f040e1a7fbd9e7da1586a31c0bca72b7af1bd5
Asia Szymczak <asiaszymczak28@gmail.com>
2018-04-24 12:37:55 +0200

    Drawer Navigator

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button, TouchableOpacity, Image, Platform, } from 'react-native';
+import { StackNavigator, DrawerNavigator, } from 'react-navigation';
+import {Icon} from 'react-native-vector-icons';
+import HomeScreen from '../HiIGSM/screens/HomeScreen';
+import MustSeeScreen from '../HiIGSM/screens/MustSeeScreen';
+import TimeTable from '../HiIGSM/screens/TimetableScreen';
 
 export default class App extends React.Component {
+
+
   render() {
-    return (
-      <View style={styles.marcka}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return ( 
+     
+      <AppDrawerNavigation/>
+       );
   }
 }
+
+const AppDrawerNavigation = DrawerNavigator({
+  HomeScreen: {screen: HomeScreen},
+    MustSeeScreen: {screen: MustSeeScreen},
+    TimeTable: {screen: TimeTable}
+
+})
 
 const styles = StyleSheet.create({
   marcka: {
--- screens/Main.js
+++ screens/Main.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class Main extends Component {
+
+class Main extends Component {
+        
     redner(){
         return(
             <Text>Hello world!</Text>
         );
     }
 }
+export default Main;
