fbe163c336b46fbda782fd6c808c7efd1ef77f2f
Asia Szymczak <asiaszymczak28@gmail.com>
2018-05-22 18:00:28 +0200

    Kuba psuje

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {StackNavigator, TabNavigator, DrawerNavigator} from 'react-navigation';
+import React, { Component } from 'react'
+import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 import nav_style from '../components/nav_style';
 import ApiKeys from '../ApiKeys'
@@ -27,16 +27,41 @@ import styles from '../components/styles';
 require("firebase/database");
 
 class HomeScreen extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = { 
-            message: ' '
+        this.state = {
+            message: ' ',
+            weekDays: []
         };
     }
-    componentDidMount(){
+    componentDidMount() {
         const rootRef = firebase.database().ref();
         const messRef = rootRef.child('message');
-     
+        const weekDaysRef = rootRef.child('weekDays');
+
+        weekDaysRef.once('value', snap => {
+            let newStateWeekDays = [];
+            snap.forEach(child => {                 // 1
+                let newEvents = {}
+                let events = child.val();
+                for (let event in events) {         // 2
+
+                    // newStateWeekDays.push({
+                    //     [weekDay]: {
+                    //         title: items[item].title,
+                    //         user: items[item].user
+                    //     }
+                    // });
+                }
+                let weekDay = child.key
+
+                this.setState({
+                    weekDays: this.state.weekDays.concat(weekDay)
+                })
+            })
+        })
+
+
 
         messRef.on('value', snap => {
             this.setState({
@@ -44,67 +69,82 @@ class HomeScreen extends Component {
             });
         });
 
+
+
     }
 
-    render(){
+    render() {
 
-            return (
-                <View style={styles.background}>
-            
-                  
-            
-            <View style={{top: 450}}>
-            
+        return (
+            <View style={styles.background}>
+
+
+
+                <View style={{ top: 450 }}>
+
                     <View style={nav_style.HomeBtn}>
-                    <TouchableOpacity onPress={()=>this.props.navigation.navigate('Home')}>
-                        <Image
-                        source={require('../assets/icons/home.png')}
-                        style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                        title='Home'
-                        />
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
+                            <Image
+                                source={require('../assets/icons/home.png')}
+                                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+                                title='Home'
+                            />
                         </TouchableOpacity>
                     </View>
 
                     <View style={nav_style.MustSeeBtn}>
-                    <TouchableOpacity onPress={()=>this.props.navigation.navigate('Map')}>
-                        <Image
-                        source={require('../assets/icons/map.png')}
-                        style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                        />
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Map')}>
+                            <Image
+                                source={require('../assets/icons/map.png')}
+                                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+                            />
                         </TouchableOpacity>
                     </View>
                     <View style={nav_style.TimeTable}>
-                    <TouchableOpacity onPress={()=>this.props.navigation.navigate('Timetable')}>
-                        <Image
-                        source={require('../assets/icons/calendar.png')}
-                        style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                        />
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Timetable')}>
+                            <Image
+                                source={require('../assets/icons/calendar.png')}
+                                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+                            />
                         </TouchableOpacity>
                     </View>
-                    </View>
-            
-                    <View>
-              
-              <View style={styles.notificationContainer}>
-              <View style={styles.notification}>
-                 {/* // <NavigationBtn/> */}
-                 
-                <Text style={styles.text}> Notifications </Text>
-              </View>
-
-              <Text style={styles.notificationsText}> {this.state.message} </Text>
-                  </View>
-
-                <View style={styles.dayContainer}>
-                 {/* // <NavigationBtn/> */}
-                <Text style={styles.text}> Wednesday </Text>
-              </View>
-
                 </View>
+
+                <View>
+
+                    <View style={styles.notificationContainer}>
+                        <View style={styles.notification}>
+                            {/* // <NavigationBtn/> */}
+
+                            <Text style={styles.text}> Notifications </Text>
+                        </View>
+
+                        <Text style={styles.notificationsText}> {this.state.message} </Text>
+                    </View>
+
+                    <View style={styles.firstContainer}>
+                        <View style={styles.day}>
+                            <Text style={styles.text}> {this.state.weekDays[0]} </Text>
+                        </View>
+                        <Text style={styles.eventText}> BREAKFAST </Text>
+                        <Text style={styles.timeText}> 8:00 - 9:30 AM </Text>
+                        <Text style={styles.placeText}> Hostel Patchwork Canteen </Text>
+                    </View>
+                    <View style={styles.greyMedium_Container}>
+                        <Text style={styles.eventText}> LECTURES </Text>
+                        <Text style={styles.timeText}> 10:00 AM </Text>
+                        <Text style={styles.placeText}> CZIiTT PW 4.05 </Text>
+                    </View>
+                    <View style={styles.greyMedium_Container}>
+                        <Text style={styles.eventText}> COFFEE BREAK </Text>
+                        <Text style={styles.timeText}> 12:30 AM </Text>
+                        <Text style={styles.placeText}> CZIiTT PW 4.05 </Text>
+                    </View>
+                </View>
+
             </View>
-                
-            )
-          
+        )
+
     }
 }
 
--- screens/MustSeeScreen.js
+++ screens/MustSeeScreen.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, ScrollView, Platform, TouchableOpacity, 
-        Image, FlatList, StatusBar} from 'react-native';
-import {CheckBox} from 'react-native-elements';
+import React, { Component } from 'react';
+import {
+  View, Text, StyleSheet, ScrollView, Platform, TouchableOpacity,
+  Image, FlatList, StatusBar
+} from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 import styles from '../components/styles';
 import nav_style from '../components/nav_style';
@@ -13,7 +15,7 @@ import Places from './Places';
 const items = ['Museums', 'Galleries', 'Monuments'];
 
 class MustSee extends Component {
- 
+
   // constructor(props) {
   //   super(props);
   //   this.state = { checked: false };
@@ -32,12 +34,12 @@ class MustSee extends Component {
       this.setState({ checked: checked.filter(a => a !== item) });
     }
   };
-  
+
   render() {
     return (
       <View style={styles.background}>
-        <StatusBar barStyle="light-content"/>
-        
+        <StatusBar barStyle="light-content" />
+
         <View>
           <FlatList
             data={items}
@@ -53,60 +55,60 @@ class MustSee extends Component {
                 uncheckedColor='#4A4A4A'
               />
             )}
-            />
+          />
         </View>
-    
+
         <View>
           <View style={styles.white_Left_Container}>
             <TouchableOpacity>
-            <View style={styles.icon}>
-              <Image source={require('../assets/icons/map.png')} style={{width: 37, height: 37 }}/> 
-            </View>
-            <Text style={styles.text2}> Show On Map </Text>
+              <View style={styles.icon}>
+                <Image source={require('../assets/icons/map.png')} style={{ width: 37, height: 37 }} />
+              </View>
+              <Text style={styles.text2}> Show On Map </Text>
             </TouchableOpacity>
           </View>
 
           <View style={styles.white_Right_Container}>
-            <TouchableOpacity onPress={()=>this.props.navigation.navigate('Places')}>
-            <View style={styles.icon}>
-              <Image source={require('../assets/icons/list.png')} style={{width: 30, height: 30}}/> 
-            </View>
-            <Text style={styles.text2}> Show List </Text>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Places')}>
+              <View style={styles.icon}>
+                <Image source={require('../assets/icons/list.png')} style={{ width: 30, height: 30 }} />
+              </View>
+              <Text style={styles.text2}> Show List </Text>
             </TouchableOpacity>
           </View>
         </View>
-        
-        
-        <View style={{top: 220}}>
-            
-            <View style={nav_style.HomeBtn}>
-              <TouchableOpacity onPress={()=>this.props.navigation.navigate('Home')}>
-                  <Image
-                  source={require('../assets/icons/home.png')}
-                  style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                  title='Home'
-                  />
-                  </TouchableOpacity>
-            </View>
 
-            <View style={nav_style.MustSeeBtn}>
-              <TouchableOpacity onPress={()=>this.props.navigation.navigate('Map')}>
-                  <Image
-                  source={require('../assets/icons/map.png')}
-                  style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                  />
-                  </TouchableOpacity>
-              </View>
-            <View style={nav_style.TimeTable}>
-              <TouchableOpacity onPress={()=>this.props.navigation.navigate('Timetable')}>
-                  <Image
-                  source={require('../assets/icons/calendar.png')}
-                  style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center'}}
-                  />
-                  </TouchableOpacity>
-            </View>
+
+        <View style={{ top: 220 }}>
+
+          <View style={nav_style.HomeBtn}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
+              <Image
+                source={require('../assets/icons/home.png')}
+                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+                title='Home'
+              />
+            </TouchableOpacity>
           </View>
-        
+
+          <View style={nav_style.MustSeeBtn}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Map')}>
+              <Image
+                source={require('../assets/icons/map.png')}
+                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={nav_style.TimeTable}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Timetable')}>
+              <Image
+                source={require('../assets/icons/calendar.png')}
+                style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
       </View>
     )
   }
