6aa9a1c6a5a2a5fc8d2541f774d216ef567a2763
Marcelina <marcelinaa.ziarno@gmail.com>
2018-06-01 12:48:06 +0200

    day screen with firebase

--- screens/DayScreen.js
+++ screens/DayScreen.js
@@ -8,34 +8,85 @@ import{
 }from 'react-native'
 import {Icon} from 'react-native-elements';
 import nav_style from '../components/nav_style';
+import * as firebase from 'firebase';
+import ApiKeys from '../ApiKeys'
+import styles from '../components/styles';
 
 class DayScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: ' ',
+            weekDays: {}
+        };
+    }
+
+    componentDidMount() {
+        const rootRef = firebase.database().ref();
+        const weekDaysRef = rootRef.child('weekDays');
+
+        weekDaysRef.once('value', snap => {
+            let newStateWeekDays = [];
+            snap.forEach(child => {     //child - dzien tyg
+                let events = child.val();
+                let weekDay = child.key
+                let newEvent = {}
+                Object.keys(events).map((key) => {
+                    let event = events[key]
+                    newEvent[key] = event
+                })
+
+                let lastState = this.state.weekDays
+
+                lastState[weekDay] = newEvent
+                let newState = lastState
+                this.setState({
+                    weekDays: newState
+                })
+            })
+        })
+        
 
     
-    
+    }
 
     render(){
         return(
             <View>
-                <TouchableOpacity onPress={() => 
-                this.props.navigation.navigate('Activity')} 
-                    style={styles.containerStyle}>    
-                        <Text style={styles.textStyleSmall}>8:00 - 9:00 AM</Text>  
-                        <Text style={styles.textStyleSmall2}>Hotel Patchwork Canteen</Text>
-                        <Text style={styles.textStyle}>BREAKFAST</Text>             
-                        
-                    
-                </TouchableOpacity> 
 
-                <TouchableOpacity onPress={() => 
-                this.props.navigation.navigate('Activity')} 
-                    style={styles.containerStyle}>    
-                        <Text style={styles.textStyleSmall}>11:00 - 14:00 AM</Text>  
-                        <Text style={styles.textStyleSmall2}>CZIiTT</Text>
-                        <Text style={styles.textStyle}>LECTURES</Text>             
-                        
-                    
-                </TouchableOpacity> 
+                 <View >
+                    {Object.keys(this.state.weekDays).map((dayNameKey) => {
+                        let dayEvents = this.state.weekDays[dayNameKey]
+                        return (
+                            
+                            
+                            <View style={styles.firstContainer}> 
+                            
+                            
+                                {Object.keys(dayEvents).map((eventName) => {
+                                    let eventData = dayEvents[eventName]
+                                    return (
+                                        
+                                        <TouchableOpacity  onPress={() => 
+                                            this.props.navigation.navigate('Activity')}>
+                                        <View style={styles.timetable_Container}>
+                                            <Text style={styles.eventText}> {eventName} </Text>
+                                            <Text  style={styles.timeText}> {eventData.startTime} - {eventData.endTime} </Text>
+                                            <Text style={styles.placeText}> {eventData.place} </Text>
+                                        </View>
+                                        </TouchableOpacity>
+                                        
+                                    )
+                                })}
+                                
+                            </View>
+                            
+
+                        )
+                    })}
+
+            </View>  
+                
 
             <View style={{top: 340, justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center'}}>
             
@@ -79,88 +130,7 @@ class DayScreen extends Component {
             
         )
     }
-}
-
-const styles = StyleSheet.create({
-    textStyle: {
-      top: '30%',
-      marginLeft: 20,
-      fontSize: 18,
-      fontWeight: 'bold',
-      color: '#4a4a4a',
-      textAlign: 'left',
-      //justifyContent: 'center',
-      flex: 1,
-      flexDirection: 'row',
-      position: 'absolute',
-      //alignItems: 'center',
-      //textAlignVertical: 'auto',
-      
-    },
-
-    textStyleSmall: {
-        //marginTop: -20,
-        right: 15,
-        top: '25%',
-        fontSize: 12,
-        //fontWeight: 'bold',
-        color: '#4a4a4a',
-        //textAlign: 'right',
-        //justifyContent: 'center',
-        flex: 1,
-        position: 'absolute',
-        //alignItems: 'center',
-        //textAlignVertical: 'center',
-        
-      },
-
-      textStyleSmall2: {
-        //marginTop: -20,
-        right: 15,
-        top: '55%',
-        fontSize: 12,
-        //fontWeight: 'bold',
-        color: '#4a4a4a',
-        //textAlign: 'right',
-        //justifyContent: 'center',
-        flex: 1,
-        position: 'absolute',
-        //alignItems: 'center',
-        //textAlignVertical: 'center',
-        
-      },
-
-
-    containerStyle: {
-      height: 60,
-      width: '90%',
-      //borderWidth: 1,
-      backgroundColor: '#ECECEC',
-      borderBottomWidth: 0,
-      borderRadius: 5, 
-
-      ...Platform.select({
-        ios: {
-          shadowColor: '#707070',
-          //shadowOffset: { height: 3 },
-          shadowOpacity: 1, //from 0 to 1
-          shadowRadius: 2,
-        },
-        android: {
-        elevation: 5
-        },
-      }),
-
-      //shadowOffset: { width: 2, height: 2 },
-    
-      marginLeft: '5%',
-      marginRight: '5%',
-      marginTop: 5,
-      marginBottom: 5,
-      alignSelf: 'center',
-      justifyContent: 'flex-end',
-    }
-})
+};
 
 //module.export = MondayScreen;
 export default DayScreen;
