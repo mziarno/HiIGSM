07e32cf5ee284e404b76822875bb6e17487248ae
KubaLobodecki <kubalobo@gmail.com>
2019-04-10 23:37:50 +0200

    Notification on HomeScreen refactor

---
+++ components/Home/MainNotification.js
@@ -0,0 +1,25 @@
+import {Subscribe} from "unstated";
+import {FirebaseContainer} from "../FirebaseContainer/FirebaseContainer";
+import {Text, TouchableOpacity, View} from "react-native";
+import React from "react";
+import styles from "../styles";
+import text_style from "../text_style";
+
+const MainNotification = props => (
+    <Subscribe to={[FirebaseContainer]}>
+        {firebase => {
+            return (
+                <TouchableOpacity onPress={() => props.navigation.navigate('Notification')}>
+                    <View style={styles.notificationContainer}>
+                        <View style={styles.notification}>
+                            <Text style={text_style.text}> Notifications </Text>
+                        </View>
+                        <Text style={text_style.notificationsText}> {firebase.state.message} </Text>
+                    </View>
+                </TouchableOpacity>
+            );
+        }}
+    </Subscribe>
+);
+
+export default MainNotification
--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,86 +1,20 @@
-import React, {Component} from 'react'
-import * as firebase from 'firebase';
-import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
-import styles from '../../components/styles';
-import text_style from '../../components/text_style'
+import React from 'react'
+import {StatusBar, View} from 'react-native';
 import TimetablePager from "../../components/Home/TimetablePager";
+import MainNotification from "../../components/Home/MainNotification";
 
-require("firebase/database");
-
-class HomeScreen extends Component {
-    constructor() {
-        super();
-        this.state = {
-            message: ' ',
-            weekDays: {},
-            iterator: 1,
-            // time: ''
-        };
-    }
-
-    componentDidMount() {
-        // Firebase connection part
-        const rootRef = firebase.database().ref();
-        const messRef = rootRef.child('message');
-        const weekDaysRef = rootRef.child('weekDays').orderByChild('id');
-        // ====== Week plan structure parser ======
-        weekDaysRef.once('value', snap => {
-            let newStateWeekDays = [];
-            snap.forEach(child => {
-                let events = child.val();
-                let weekDay = child.key;
-                let newEvent = {};
-
-                Object.keys(events).map((eventKey) => {
-                    let event = events[eventKey];
-                    let newInfos = {};
-
-                    if (typeof event === "object") { //Sometimes event is type of number and other strange things - probably problem with data
-                        Object.keys(event).map((eventInfoKey) => {
-                            newInfos[eventInfoKey] = event[eventInfoKey];
-                        })
-                        newEvent[eventKey] = event;
-                    }
-                })
-                let lastState = this.state.weekDays
-                lastState[weekDay] = newEvent
-                let newState = lastState
-                this.setState({
-                    weekDays: newState
-                })
-            })
-        })
-        // ====== Parser end ======
-        messRef.on('value', snap => {
-            this.setState({
-                message: snap.val()
-            });
-        });
-    }
-
-    render() {
-        const {navigate} = this.props.navigation;
-
-        return (
-            <View>
-                <StatusBar barStyle="light-content"/>
-                <View style={{height: '90%'}}>
-                    <TouchableOpacity onPress={() => navigate('Notification')}>
-                        <View style={styles.notificationContainer}>
-                            <View style={styles.notification}>
-                                <Text style={text_style.text}> Notifications </Text>
-                            </View>
-                            <Text style={text_style.notificationsText}> {this.state.message} </Text>
-                        </View>
-                    </TouchableOpacity>
-                    <View>
-                        <TimetablePager navigation={this.props.navigation}/>
-                    </View>
-
+const HomeScreen = props => {
+    return (
+        <View>
+            <StatusBar barStyle="light-content"/>
+            <View style={{height: '90%'}}>
+                <MainNotification navigation={props.navigation}/>
+                <View>
+                    <TimetablePager navigation={props.navigation}/>
                 </View>
             </View>
-        )
-    }
-}
+        </View>
+    )
+};
 
 export default HomeScreen;
