fa322b2e963f823bdb0aacbb50ebc69602d22df3
KubaLobodecki <kubalobo@gmail.com>
2019-04-10 23:23:21 +0200

    New HomeScreen - timetable

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
-import {IndicatorViewPager, PagerDotIndicator} from 'rn-viewpager';
 import * as firebase from 'firebase';
-import {ScrollView, StatusBar, Text, TouchableOpacity, View,} from 'react-native';
+import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
 import styles from '../../components/styles';
 import text_style from '../../components/text_style'
+import TimetablePager from "../../components/Home/TimetablePager";
 
 require("firebase/database");
 
@@ -19,8 +19,6 @@ class HomeScreen extends Component {
     }
 
     componentDidMount() {
-        //  this.Clock = setInterval( () => this.GetTime(), 1000 );
-
         // Firebase connection part
         const rootRef = firebase.database().ref();
         const messRef = rootRef.child('message');
@@ -62,66 +60,7 @@ class HomeScreen extends Component {
 
     render() {
         const {navigate} = this.props.navigation;
-        if (this.state.weekDays === 0) {
-            return null;
-        }
-        var pageViews = [];
 
-        let weekDays = this.state.weekDays;
-
-        let i = 0;
-        Object.keys(weekDays).map(function (dayNameKey, index) {
-            let events = weekDays[dayNameKey];
-            let eventsArray = [];
-            //console.log(events)
-
-            Object.keys(events).map(function (eventNameKey, index) {
-                var singleEvent = events[eventNameKey];
-                var eventInfosArray = [];
-
-                let time = " - "
-                let place = ''
-
-                Object.keys(singleEvent).map(function (eventInfoKey, index) {
-                    // ===== Event info display =====                        
-                    if (eventInfoKey == 'startTime') {
-                        time = singleEvent[eventInfoKey] + time
-                    } else if (eventInfoKey == 'endTime') {
-                        time = time + singleEvent[eventInfoKey]
-                    } else if (eventInfoKey == 'place') {
-                        place = singleEvent[eventInfoKey]
-                    }
-                })
-                eventInfosArray.push(
-                    <Text style={text_style.timeText}>{time}</Text>,
-                    <Text style={text_style.placeText}>{place}</Text>
-                );
-                eventsArray.push(
-                    // ===== Event card =====
-                    <TouchableOpacity onPress={() => navigate('Activity', {activity: events[eventNameKey]})}>
-                        <View style={styles.whiteMedium_Container}>
-                            <Text style={text_style.eventText}>{eventNameKey}</Text>
-                            {eventInfosArray}
-                        </View>
-                    </TouchableOpacity>
-                );
-            })
-            pageViews.push(
-                // ===== Day page =====
-                <View>
-                    <View style={styles.day}>
-                        <Text style={text_style.text}>{dayNameKey}</Text>
-                    </View>
-                    <View style={{height: '80%'}}>
-                        <ScrollView>
-                            {eventsArray}
-                        </ScrollView>
-                    </View>
-                </View>
-            )
-            i += 1;
-        })
-        var pageCount = pageViews.length;
         return (
             <View>
                 <StatusBar barStyle="light-content"/>
@@ -135,21 +74,12 @@ class HomeScreen extends Component {
                         </View>
                     </TouchableOpacity>
                     <View>
-                        {/* WeekDays pages renderer */}
-                        <IndicatorViewPager
-                            key={pageCount}
-                            style={styles.timetable_background}
-                            indicator={this._renderDotIndicator()}>
-                            {pageViews}
-                        </IndicatorViewPager>
+                        <TimetablePager navigation={this.props.navigation}/>
                     </View>
+
                 </View>
             </View>
         )
-    }
-
-    _renderDotIndicator() {
-        return <PagerDotIndicator pageCount={7}/>;
     }
 }
 
