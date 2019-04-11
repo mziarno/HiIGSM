56e272548612afcb21609431d9105232d8540998
KubaLobodecki <kubalobo@gmail.com>
2019-04-12 00:21:43 +0200

    Header in MapScreen

---
+++ router/Drawer/MapStack.js
@@ -0,0 +1,21 @@
+import {createStackNavigator} from "react-navigation";
+import React from "react";
+import {headerNavigationOptions} from "../routerTools";
+import MapScreen from "../../screens/MapScreen";
+
+/*
+* Stack which makes it possible to attach additional screens available only from MapScreen
+* */
+
+const MapStack = createStackNavigator({
+    Home: {
+        screen: MapScreen,
+        navigationOptions: {
+            title: 'IGSM map',
+        }
+    },
+}, {
+    defaultNavigationOptions: headerNavigationOptions
+});
+
+export default MapStack
