c0c43ad911d352d6786e70c7fb42fa1617593108
Alicja <alicja.konkol@gmail.com>
2019-06-20 12:47:55 +0200

    Abstracts proper scroll

--- screens/AbstractsScreen.js
+++ screens/AbstractsScreen.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {StatusBar, View} from 'react-native';
-import AllNotifications from "../components/Notifications/AllNotifications";
 import AllPosters from "../components/Abstracts/AllPosters";
 
 const AbstractsScreen = props => {
     return (
         <View>
             <StatusBar barStyle="light-content"/>
-            <View style={{height: '90%'}}>
+            <View>
                 <AllPosters navigation={props.navigation}/>
             </View>
         </View>
