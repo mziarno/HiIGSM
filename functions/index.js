2c3a36ec9d5cdbce69bf7db45a8be90b7f2e835e
KubaLobodecki <kubalobo@gmail.com>
2019-10-17 23:54:59 +0200

    Too many notifications hotfix

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
+var pako = require('pako');
+
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -7,6 +9,7 @@ admin.initializeApp(functions.config().firebase);
 exports.sendPushNotification = functions.database.ref('notifications/{id}').onCreate((change, context) => {
 
     console.log("DZIAŁA!");
+    console.log(change.val().content);
 
     const root = admin.database().ref();
     const messages = [];
@@ -21,21 +24,31 @@ exports.sendPushNotification = functions.database.ref('notifications/{id}').onCr
             if (expoToken) {
                 messages.push({
                     "to": expoToken,
-                    "body": "New notification about IGSM!"
+                    "body": change.val().content
                 })
             }
         });
         return Promise.all(messages)
     }).then(messages => {
+        const groupSize = 50;
 
-        fetch('https://exp.host/--/api/v2/push/send', {
+        for(i = 0; i < messages.length / groupSize; i++) {
+            fetch('https://exp.host/--/api/v2/push/send', {
             method: "POST",
             headers: {
                 "Accept": "application/json",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "host": "exp.host",
+                "accept-encoding": "gzip, deflate",
             },
-            body: JSON.stringify(messages)
-        })
+            body: JSON.stringify(messages.slice(i*groupSize, i*groupSize+groupSize))
+
+            }).then(
+                (val) => (console.log(val)),
+                (reason) => (console.log("BBB" + reason))
+            )
+            }
+
     })
 });
 
